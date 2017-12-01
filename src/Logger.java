551a7a10ecde6268c79d8998e95e34bd85e6db6e
kedington <kev4725@gmail.com>
2017-11-30 21:43:23 -0800

    Updated readme and logger now gets class name that called logger

--- src/Logger.java
+++ src/Logger.java
@@ -17,10 +17,15 @@ public class Logger {
         Logger.fileName = fileName;
     }
 
+    /**
+     * Extract the class name of the method that called the logger and the time.
+     * Add the Message to the buffer to be written to the file.
+     * @param tag       An indicator to represent the severity of the log message
+     * @param content   The body of the log message.
+     */
     public static void writeToTextFile(char tag, String content) {
         try {
             String className = new Exception().getStackTrace()[1].getClassName();
-
             String msg = Long.toString(System.currentTimeMillis()) + " " + tag + " " + content + " " + className + '\n';
             FileOutputStream out = new FileOutputStream(fileName, true);
             out.write(msg.getBytes());
